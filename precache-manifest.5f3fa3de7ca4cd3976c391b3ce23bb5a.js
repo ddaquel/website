be9b7171cb88b92310010228b46ea674ad463714
ddaquel <z5160566@ad.unsw.edu.au>
2020-02-10 23:09:49 +1100

    change gif

---
+++ precache-manifest.5f3fa3de7ca4cd3976c391b3ce23bb5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0abd88334fdf01e6c1bda9080698c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e59d873c0da7f957b0fa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85dcb58fca5eb3b9ccef",
+    "url": "/static/js/2.7d070754.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7d070754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59d873c0da7f957b0fa",
+    "url": "/static/js/main.05c5701e.chunk.js"
+  },
+  {
+    "revision": "2c55f46893576a7484a4",
+    "url": "/static/js/runtime-main.e64d837f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90c60c1596be4f9e71a857df94a5b7b8",
+    "url": "/static/media/random-spinner.90c60c15.gif"
+  }
+]);
