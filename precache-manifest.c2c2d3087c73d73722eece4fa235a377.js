bc614c7f75ba43cc03c7d3357d2711151f79e7c9
ddaquel <z5160566@ad.unsw.edu.au>
2020-02-10 21:54:22 +1100

    attempt to put react app as home

---
+++ precache-manifest.c2c2d3087c73d73722eece4fa235a377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6f1e8e1d67e22426566",
+    "url": "/resume/static/css/main.38d0a085.chunk.css"
+  },
+  {
+    "revision": "e6f1e8e1d67e22426566",
+    "url": "/resume/static/js/main.a7d83cea.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/resume/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "98933a698d803ed903b3430c7d2fe4cd",
+    "url": "/resume/index.html"
+  }
+];
