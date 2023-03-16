64dd49a1450b39669df65cc235f9707bf24e571e
ddaquel <derekdaquel@gmail.com>
2023-03-16 21:10:25 +1100

    redo: make simple

--- src/helpers/aspectRatio.ts
+++ src/helpers/aspectRatio.ts
@@ -1,8 +1,4 @@
-export const ASPECT_RATIO_CURRENT_WORK_LOGO = 1280 / 467;
 export const ASPECT_RATIO_MY_PHOTO = 1;
-export const ASPECT_RATIO_BACKGROUND = 4032 / 2268;
-export const ASPECT_JOB_LOGO = 1;
-export const ASPECT_BRAND_LOGO = 962 / 160;
 
 export const calculateScaledHeight = (
   width: number,
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,6 @@ module.exports = {
         laptop: "1080px", // => @media (min-width: 1080px) { ... }
         desktop: "1280px", // => @media (min-width: 1280px) { ... }
       },
-      colors: {
-        black: "#000000",
-        "primary-blue": "#41B6E6",
-        "primary-pink": "F884D9",
-        grey: "grey",
-      },
-      backgroundImage: {
-        "primary-gradient": "linear-gradient(to right, #41b6e6, #f884d9)",
-      },
     },
   },
   plugins: [],
